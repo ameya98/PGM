5ff9cda77b392fc4b0c003e91b78d607d0e6bb2f
Ameya Daigavane <ameya.d.98@gmail.com>
2018-09-28 00:56:15 +0530

    rename classes, and text review

--- simple_pgm.py
+++ simple_pgm.py
@@ -6,26 +6,23 @@ def invert(d):
         inv_d = {}
         for vertex, edge_list in d:
             for neighbour in edge_list:
-                if not neighbour in inv_d:
+                if neighbour not in inv_d:
                     inv_d[neighbour] = []
-
                 inv_d[neighbour].append(vertex)
-
     except TypeError:
         raise
 
 
-class bayesian_network:
-
+class BayesianNetwork:
     def __init__(self, vertices, ancestor_dict, distribution_dict):
         self.vertices = []
         for vertex in vertices:
-            self.vertices.append(bayesian_node(self, vertex, ancestor_dict[vertex], distribution_dict[vertex]))
+            self.vertices.append(BayesianNode(self, vertex, ancestor_dict[vertex], distribution_dict[vertex]))
 
         self.edges = invert(ancestor_dict)
 
-class bayesian_node:
 
+class BayesianNode:
     def __init__(self, population, name, ancestors, distribution):
         self.population = population
         self.name = name
